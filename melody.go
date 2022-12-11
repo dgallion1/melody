1c2e1e7767b4343cb91ba886880e8964525edde8
Darrell <dgallion1@gmail.com>
2022-12-11 09:57:04 -0500

    Removed larger buffer

--- melody.go
+++ melody.go
@@ -72,7 +72,7 @@ type Melody struct {
 func New() *Melody {
 	upgrader := &websocket.Upgrader{
 		ReadBufferSize:  1024,
-		WriteBufferSize: 1024 * 10,
+		WriteBufferSize: 1024,
 		CheckOrigin:     func(r *http.Request) bool { return true },
 	}
 
